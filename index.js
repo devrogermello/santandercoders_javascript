63c5e7ee2700d24e68e5f563058eccf8afe2fad0
Roger Mello <roger.empresas@hotmail.com>
2024-06-16 08:24:09 -0300

    Aprendendo variáveis - Curso Santander Coders

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-console.log('Hello World!')
-console.log('Estamos na nossa aula de JavaScript!')
+//utilize sempre o Canel case!
+
+var nomeDoProfessor = "Roger Mello"; //string (texto)
+var idade = 27; //number
+var altura = 1.77; //number
+var estudando = false // boolean (booleano: verdadeiro ou falso)
+
+console.log(nomeDoProfessor, typeof nomeDoProfessor)
+console.log(idade, typeof idade)
+console.log(altura, typeof altura)
+console.log(estudando, typeof estudando)
+
+var semConteudo;
+
+console.log(semConteudo)
+
+var curso = "Front-end em React",
+  topico = "JavaScript Básico I";
+
+console.log(curso, topico)
+
+let notaDoAluno = 10;
+const mediaDoAluno = 8;
+
+notaDoAluno = 9;
+// ! mediaDoAluno = 5; Não pode alterar variavel const!
+
+console.log(notaDoAluno)
+console.log(mediaDoAluno)
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let notaDoAluno = 10;
+const mediaDoAluno = 8;
+
+notaDoAluno = 9;
+// ! mediaDoAluno = 5; Não pode alterar variavel const!
+
+console.log(notaDoAluno)
+console.log(mediaDoAluno)
