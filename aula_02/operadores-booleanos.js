3a935235f2dab3ad7b5543cb42220739a60b500f
Roger Mello <roger.empresas@hotmail.com>
2024-06-16 09:10:09 -0300

    Operadores Booleanos e Conjunções Lógicas - Santrander Coders

---
+++ aula_02/operadores-booleanos.js
@@ -0,0 +1,20 @@
+// Operadores Booleanos: Comparações
+
+const numero1 = 10;
+const numero2 = 12;
+
+console.log(numero1 == numero2);
+console.log(numero1 === numero2);
+
+console.log(numero1 !== numero2);
+console.log(numero1 >= numero2);
+
+//Conjunções Lógicas
+
+const idadePessoa1 = 16;
+const idadePessoa2 = 39;
+
+console.log(idadePessoa1 >= 18 && idadePessoa2 >= 18)
+console.log(idadePessoa1 >= 18 || idadePessoa2 >= 18)
+
+console.log(!(idadePessoa1 >= 18));
---
+++ aula_02/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const idadePessoa1 = 16;
+const idadePessoa2 = 39;
+
+console.log(idadePessoa1 >= 18 && idadePessoa2 >= 18)
+console.log(idadePessoa1 >= 18 || idadePessoa2 >= 18)
+
+console.log(!(idadePessoa1 >= 18));
